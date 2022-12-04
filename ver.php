96768a1b47049599bce5cfe98d49a3f6fdc55f4f
Dieeh <diego.paz@hotmail.com>
2022-12-04 19:43:00 -0300

    askjdfkjhfsdgjlkasñdh

--- ver.php
+++ ver.php
@@ -4,6 +4,7 @@
     require "conexion.php";
 
     $peli_id = $_GET['view'];
+    
 
     if (isset($_SESSION['user_id'])) {
         $records = $conexion->prepare('SELECT id, username, password FROM users WHERE id = :id');
@@ -18,10 +19,35 @@
         }
     }
 
+    $wished = $conexion->prepare('SELECT * FROM wishlist WHERE id_movie = :peli AND wisher = :user');
+    $wished->bindParam(':peli',$peli_id);
+    $wished->bindParam(':user',$_SESSION['user_id']);
+    $wished->execute();
+    $res1 = $wished->fetchAll(PDO::FETCH_ASSOC);
+    $res = count($res1);
+
     $data = $conexion->prepare('SELECT * FROM movies_carac WHERE id_movie = :id');
     $data->bindParam(':id', $peli_id);
     $data->execute();
-    $row = $data->fetch(PDO::FETCH_ASSOC)
+    $row = $data->fetch(PDO::FETCH_ASSOC);
+
+    if (isset($_GET['wish'])) {
+        $id_mov = $_GET['wish'];
+        $usr = $_SESSION['user_id'];
+        $ins = $conexion->prepare("INSERT INTO wishlist (id_movie, wisher) VALUES ('$id_mov','$usr')");
+        $ins->execute();
+        $string = "Location: ver.php?view=$id_mov";
+        header($string);
+    }
+
+    if (isset($_GET['delete'])) {
+        $peli_del = $_GET['delete'];
+        $usr = $_SESSION['user_id'];
+        $del = $conexion->prepare("DELETE FROM wishlist WHERE id_movie = $peli_del AND wisher = $usr");
+        $del->execute();
+        $string = "Location: ver.php?view=$peli_del";
+        header($string);
+    }
 ?>
 
 
@@ -47,8 +73,13 @@
             <h3 style="font-size: 20px; text-transform: capitalize; margin-bottom: 25px; letter-spacing: 2px;"><?php echo $row['gender'];?> · <?php echo $row['public'];?> · <?php echo $row['lenght'];?>m</h3>
             <p style="font-size: 18px; line-height: 30px; margin-bottom: 10px; letter-spacing: 1px;">Starring: <?php echo $row['cast'];?></p>
             <p style="font-size: 15px; line-height: 30px; margin-bottom: 10px; letter-spacing: 1px;"><?php echo $row['description'];?></p>
-            <a class="btn" href="ver.php?view=<?php echo $movies['id_movie']; ?>">Add to wishlist</a>
-            <a class="btn" href="ver.php?view=<?php echo $movies['id_movie']; ?>">Rent</a>
+            <?php if($res > 0): ?>
+                <a class="btn2" href="ver.php?delete=<?php echo $row['id_movie']; ?>">Remove from wishlist</a>
+            <?php else: ?>
+                <a class="btn" href="ver.php?wish=<?php echo $row['id_movie']; ?>">Add to wishlist</a>
+            <?php endif; ?>
+            <a class="btn" href="ver.php?view=<?php echo $row['id_movie']; ?>">Rent</a>
+            <a class="btn2" href="ver.php?return=<?php echo $row['id_movie']; ?>">Return</a>
         </div>
     </div>
 
