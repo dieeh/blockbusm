7493d30844d25dc0c2b49467b82134b6e68758d7
Dieeh <diego.paz@hotmail.com>
2022-12-05 01:13:03 -0300

    jeje

--- profile.php
+++ profile.php
@@ -32,11 +32,24 @@
     }
 
     if (isset($_GET['view'])){
-        $viewed_user = $_GET['view']
+        $viewed_user = $_GET['view'];
         $viewer = $conexion->prepare("SELECT * FROM users WHERE id = $viewed_user");
         $viewer->execute();
-        $resultados = $viewer->fetchAll(PDO::FETCH_ASSOC);
-    }
+        $resultados = $viewer->fetch(PDO::FETCH_ASSOC);
+        $viewer2 = $conexion->prepare("SELECT * FROM rented_movies WHERE renter = $viewed_user");
+        $viewer2->execute();
+        $resultados2 = $viewer2->fetchAll(PDO::FETCH_ASSOC);
+        $num = count($resultados2);
+
+        $viewer3 = $conexion->prepare("SELECT * FROM followers WHERE follower_id = $viewed_user");
+        $viewer3->execute();
+        $resultados2 = $viewer3->fetchAll(PDO::FETCH_ASSOC);
+        $seguidos = count($resultados2);
+        $viewer4 = $conexion->prepare("SELECT * FROM followers WHERE following_id = $viewed_user");
+        $viewer4->execute();
+        $resultados5 = $viewer4->fetchAll(PDO::FETCH_ASSOC);
+        $seguidores = count($resultados5);
+    }   
 ?>
 
 
@@ -54,9 +67,14 @@
 </head>
 <body>
     <?php require "partials/header.php" ?>
-    <div class = "inf">
-        <div class = "inf"><b><?php echo $totalS; ?></b> seguidores </div>
-        <div class = "inf"><b><?php echo $totalC; ?></b> seguidos </div>
+
+    <div class="main" style="width: 1130px; max-width: 95%; margin: 0 auto; display: flex; align-items: center; justify-content: space-around; padding: 30px;">
+        <img src="assets/img/icon-profile.png" style="height: auto; width: 200px;">
+        <div class="about-text" style="width: 550px;">
+            <h1 style="font-size: 60px; text-transform: capitalize; margin-bottom: 20px;"><?php echo $resultados['username'];?></h1>
+            <h3 style="font-size: 20px; margin-bottom: 25px; letter-spacing: 2px;"><?php echo $resultados['total_rented'];?> movies rented in total · <?php echo $num;?> movies rented now</h3>
+            <p style="font-size: 18px; line-height: 30px; margin-bottom: 10px; letter-spacing: 1px;"><?php echo $seguidores;?> followers · following <?php echo $seguidos;?></p>
+        </div>
     </div>
 </body>
 </html>
